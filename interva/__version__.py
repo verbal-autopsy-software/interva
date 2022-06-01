8531af9db2dead5ddeab899f8097afc6ac5877e5
jarathomas <jarathomas@gmail.com>
2022-06-01 13:14:39 -0400

    adding data files and setup

---
+++ interva/__version__.py
@@ -0,0 +1,7 @@
+__title__ = "interva"
+__description__ = "Python implementation of the InterVA Algorithm."
+__url__ = "https://github.com/verbal-autopsy-software/interva"
+__version__ = "0.0.1"
+__author__ = "Jason Thomas, Yue Chu, Sherry Zhao"
+__author_email__ = "jarathomas@gmail.com"
+__license__ = "GPLv3"
